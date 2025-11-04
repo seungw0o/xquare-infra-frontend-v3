b9ee6a12b2d85196144730057117f227318f2013
seungW0o <ssounsowns@gmail.com>
2025-11-04 20:11:55 +0900

    width 속성 추가

--- libs/xquare-user-interface/src/components/noticeHeader/noticeHeader.tsx
+++ libs/xquare-user-interface/src/components/noticeHeader/noticeHeader.tsx
@@ -1,11 +1,11 @@
 import styled from "@emotion/styled";
 import { Props } from "./noticeHeader.types";
 
-const Wrapper = styled.div`
+const Wrapper = styled.div<Pick<Props, "width">>`
   display: flex;
   flex-direction: column;
   gap: 5px;
-  width: 664px;
+  width: ${({ width }) => width || "664px"};
   border-bottom: 1px solid ${({ theme }) => theme.color.text.none.gray.dark};
   padding-bottom: 10px;
   cursor: pointer;
@@ -22,9 +22,9 @@ const Date = styled.div`
   color: ${({ theme }) => theme.color.text.none.gray.dark};
 `;
 
-export const NoticeHeader = ({ title, date }: Props) => {
+export const NoticeHeader = ({ title, date, width }: Props) => {
   return (
-    <Wrapper>
+    <Wrapper width={width}>
       <Title className="title">{title}</Title>
       <Date>{date}</Date>
     </Wrapper>
